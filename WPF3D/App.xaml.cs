13468cb86e6ffa8fa9519a176ca0e33eb119b03f
Heisen <heisencodes@outlook.com>
2026-01-02 13:03:24 +0530

    added project

---
+++ WPF3D/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WPF3D.ViewModels;
+using WPF3D.Views;
+
+namespace WPF3D
+{
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+            host.Start();
+
+            App app = new();
+            app.InitializeComponent();
+            app.MainWindow = host.Services.GetRequiredService<MainWindow>();
+            app.MainWindow.Visibility = Visibility.Visible;
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Register your services here
+                services.AddSingleton<MainWindow>();
+                services.AddSingleton<MainWindowViewModel>();
+
+            });
+    }
+}
---
+++ WPF3D/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using WPF3D.ViewModels;
+
+namespace WPF3D.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainWindowViewModel viewModel)
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+    }
+}
